f31a81136904c49fbf13ccaef270f6ada2907db1
plasma712 <plasma712@gmail.com>
2019-02-02 17:34:28 +0900

    01

---
+++ Assets/Helpdel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helpdel : MonoBehaviour
+{
+    public GameObject Help;
+
+    public float k = 0;
+	// Update is called once per frame
+	void Update ()
+    {
+        k += Time.deltaTime;
+
+        if(k>4)
+        {
+            Help.gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Assets/backAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backAudio : MonoBehaviour {
+    public SoundManager SoundM;
+    private AudioSource myAudio;
+	// Use this for initialization
+	void Start () {
+        myAudio = gameObject.GetComponent<AudioSource>();
+        myAudio.PlayOneShot(SoundM.BackGround);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
